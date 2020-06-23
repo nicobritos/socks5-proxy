5bd114d6ac03d2b57749569600024739c19fc1ee
Agustín Roca <aroca@itba.edu.ar>
2020-06-23 14:54:05 -0300

    auth_server_response_parser

--- src/monitor/parser/client/auth_server_response_parser.h
+++ src/monitor/parser/client/auth_server_response_parser.h
@@ -17,13 +17,18 @@ struct auth_response {
     uint8_t status;
     char * message;
     parser_error_t error;
+    struct parser * parser;
+    size_t message_length;
+    uint8_t finished;
 };
+
+struct auth_response * auth_response_parser_init();
 
 /**
  * Dado un datagrama (array de bytes) de autentificacion para el proxy y su longitud, parsea el datagrama
  * Si no cumple con el RFC devuelve INVALID_INPUT_FORMAT_ERROR en el campo de error de la estructura.
  */
-struct auth_response * auth_response_parser(uint8_t *s, size_t length);
+struct auth_response * auth_response_parser_consume(uint8_t *s, size_t length, struct auth_response * auth_response);
 
 /**
  * Libera la memoria utilizada por la estructura, si auth_response es NULL, no hace nada
